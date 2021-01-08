7665aeb0af4e58e08c949c04ce0176a8d1e2975d
AnonymousICT <kuohoward@gmail.com>
2021-01-07 21:01:24 -0800

    added user input

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -7,13 +7,24 @@ public class NumberWizard : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        int max = 1000;
+        int min = 1;
+        int guess= 500;
+
+        Debug.Log("this is the new attempt");
         Debug.Log("Welcome to Number Wizard");
-        Debug.Log("Pick a number between 0-1000");
+        Debug.Log("Pick a number between " + min + "-" +max);
+        Debug.Log("Tell me if your number is higher or lower than " + guess);
+        Debug.Log("Up = Higher Down = lower Enter=Correct" );
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
+            Debug.Log("Up arrow key was pressed");
+
+        if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
+        Debug.Log("Down arrow key was pressed");
     }
 }
